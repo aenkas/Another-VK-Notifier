d3b1d20e370bd28177a64a268c977983a87462f1
paulpaul1076 <paulpaul1076@gmail.com>
2016-02-27 20:13:27 +0300

    .

--- AVKN/MsgReceiver.cs
+++ AVKN/MsgReceiver.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using VkNet;
+using VkNet.Categories;
+using VkNet.Utils;
+using VkNet.Model;
+using VkNet.Enums;
+using VkNet.Exception;
+using VkNet.Enums.Filters;
+using System.Windows.Forms;
 
 namespace AVKN
 {
@@ -9,7 +17,25 @@ namespace AVKN
     {
         public bool LogInVk(string login, string password)
         {
-            return false;
+            try
+            {
+                var auth = new ApiAuthParams();
+
+                Settings scope = Settings.All;
+                VkApi vk = new VkApi();
+                auth.Login = login;
+                auth.Password = password;
+                auth.ApplicationId = 5322484;
+                auth.Settings = scope;
+
+                vk.Authorize(auth);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+            return true;
         }
 
         public bool IsLogged()
